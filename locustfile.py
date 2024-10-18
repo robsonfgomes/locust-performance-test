d42bf49b608fd2804bf705cea68137b3f9fa7078
Robson Gomes <robson.gomes@newrizon.global>
2024-10-18 16:50:41 -0300

    fix: Correção docker compose.

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,8 @@
 from locust import HttpUser, task
+# from generate_pokemons import get_random
 
-class Pokemon(HttpUser):
+class Pokemon(HttpUser):    
     @task
     def list(self):
-        # Substitua pelo ip da máquina
-        self.client.get("http://host.docker.internal:4444/pokemon") 
+        # Endereço da API no docker
+        self.client.get("http://node-api:4444/pokemon")
